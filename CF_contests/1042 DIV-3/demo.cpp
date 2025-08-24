48cd7c9f387f100261a15aed9e677fd5732e2b18
VAMSI KRISHNA <vamsidayyala@gmail.com>
2025-08-24 12:47:10 +0530

    A) Lever.cpp

--- CF_contests/1042 DIV-3/demo.cpp
+++ CF_contests/1042 DIV-3/demo.cpp
@@ -1,17 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
 
 void solve(){
-    
-}
+    int n;
+    cin>>n;
+    int arr[n],brr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    for(int i=0;i<n;i++) cin>>brr[i];
+    int cnt=1;
+    for(int i=0;i<n;i++){
+        if(arr[i]>brr[i]) cnt+=(arr[i]-brr[i]);
+    }
+    cout<<cnt<<endl;
 
-signed main(){
-    ios_base::sync_with_stdio(0);
+}
+int main(){
+    ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
+
     int t=1;
     cin>>t;
-    while(t--) solve();
+    while(t--){
+        solve();
+    } 
+
     return 0;
 }
