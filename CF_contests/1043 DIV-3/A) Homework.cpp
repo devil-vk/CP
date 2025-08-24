e0a8c675c2cf5385253e3712a2f32bdfa4b9dcf6
VAMSI KRISHNA <vamsidayyala@gmail.com>
2025-08-24 12:51:50 +0530

    Update and rename demo.cpp to A) Homework.cpp

--- CF_contests/1043 DIV-3/A) Homework.cpp
+++ CF_contests/1043 DIV-3/A) Homework.cpp
@@ -4,6 +4,22 @@ using namespace std;
 #define endl '\n'
 
 void solve(){
+    int n;
+    cin>>n;
+    string a;
+    cin>>a;
+    int m;
+    cin>>m;
+    string b,c;
+    cin>>b>>c;
+    for(int i=0;i<m;i++){
+        if(c[i]=='V'){
+            a=b[i]+a;
+        }else{
+            a+=b[i];
+        }
+    }
+    cout<<a<<endl;
     
 }
 
